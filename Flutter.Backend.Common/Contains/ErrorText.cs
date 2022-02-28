c5928b0a309803840754a2d47ffe5b8c0225faf2
Nguyen Huu Tri <tri.nguyen@dgvdigital.com>
2022-02-28 13:48:25 +0700

    Set up database - error message text key

---
+++ Flutter.Backend.Common/Contains/ErrorText.cs
@@ -0,0 +1,10 @@
+﻿namespace Flutter.Backend.Common.Contains
+{
+    public static class ErrorText
+    {
+        public const string ERR_MSG_USERNAME_OR_PASSWORD_INCORRECT = "ERR_MSG_USERNAME_OR_PASSWORD_INCORRECT";
+
+
+
+    }
+}
---
+++ Flutter.Backend.DAL/Contexts/IResposibity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flutter.Backend.DAL.Contexts
+{
+    public interface  IResposibity<T> where T : class
+    {
+        void Add(T item);
+
+        void Update(T item);
+    }
+}
