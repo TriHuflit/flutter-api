7e887bfc01ef8b3857a98f1aa4ad0c54526a23d2
Nguyen Huu Tri <tri.nguyen@dgvdigital.com>
2022-05-19 13:35:08 +0700

    add controller category and brand

--- Flutter.Backend.Api/Controllers/CategoryController.cs
+++ Flutter.Backend.Api/Controllers/CategoryController.cs
@@ -1,4 +1,7 @@
-﻿using Flutter.Backend.Service.IServices;
+﻿using Flutter.Backend.Common.Constains;
+using Flutter.Backend.Service.IServices;
+using Flutter.Backend.Service.Models.Requests;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -7,6 +10,7 @@ namespace Flutter.Backend.Api.Controllers
 {
     [Route("api/category")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = RoleConstain.MANAGER)]
     public class CategoryController : ControllerBase
     {
         private readonly ICategoryService _categoryService;
@@ -16,12 +20,67 @@ namespace Flutter.Backend.Api.Controllers
             _categoryService = categoryService;
         }
 
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         [Route("all")]
+        [AllowAnonymous]
         public async Task<IActionResult> GetAllCategoryAsync()
         {
             var result = await _categoryService.GetAllCategoriesAsync();
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("create")]
+        public async Task<IActionResult> CreateCategoryAsync(BaseCategoryRequest request)
+        {
+            var result = await _categoryService.CreateCategoriesAsync(request);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("update")]
+        public async Task<IActionResult> UpdateCategoryAsync(UpdateCategoryRequest request)
+        {
+            var result = await _categoryService.UpdateCategoriesAsync(request);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="CategoryId"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("detele/{CategoryId}")]
+        public async Task<IActionResult> DeleteCategoryAsync(string CategoryId)
+        {
+            var result = await _categoryService.DeleteCategoriesAsync(CategoryId);
             if (result.IsSuccess)
             {
                 return Ok(result);
